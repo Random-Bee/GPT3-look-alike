3a849efef367c93320953f1413af16b2bb1cd6ce
Random-Bee <co21btech11002@iith.ac.in>
2023-03-08 13:19:23 +0530

    .

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,69 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri'
+import logo from '../../assets/logo.svg'
+
+// BLEM -> Block Element Modifier
+
+const Menu = () => {
+  return (
+    <>
+      <p>
+        <a href="#home">Home</a>
+      </p>
+      <p>
+        <a href="#wgpt3">What is GPT3</a>
+      </p>
+      <p>
+        <a href="#possibility">Closed AI</a>
+      </p>
+      <p>
+        <a href="#features">Case Studies</a>
+      </p>
+      <p>
+        <a href="#blog">Libraries</a>
+      </p>
+    </>
+  )
+}
+
 
 const Navbar = () => {
+
+  const [toggleMenu, setToggleMenu] = useState(false);
+
   return (
-    <div>Navbar</div>
+    <div className='gpt3__navbar'>
+      <div className="gpt3__navbar-links">
+        <div className="gpt3__navbar-links_logo">
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="gpt3__navbar-links_container">
+          <Menu />
+        </div>
+      </div>
+      <div className="gpt3__navbar-sign">
+        <p>Sign In</p>
+        <button type='button'> Sign Up</button>
+      </div>
+      <div className="gpt3__navbar-menu">
+        {toggleMenu
+          ? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)} />
+          : <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)} />
+        }
+        {toggleMenu && (
+          <div className='gpt3__navbar-menu_container slide-in-elliptic-bottom-fwd'>
+            <div className="gpt3__navbar-men_container-links">
+              <Menu />
+            </div>
+            <div className="gpt3__navbar-menu_container-links-sign">
+              <p>Sign in</p>
+              <button type="button">Sign up</button>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
   )
 }
 
